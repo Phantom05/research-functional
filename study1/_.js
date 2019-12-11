6462f15b1e93c47612c5453fa17774c36da2874d
phantom05 <monster2jy@gmail.com>
2019-12-11 11:02:46 +0900

    업데이트 : 파이프라인 복습 하기

--- study1/_.js
+++ study1/_.js
@@ -34,10 +34,45 @@ function _curryr(fn){
   }
 }
 
+// get
 var _get = _curryr(function(obj,key){
   return obj == null ? undefined : obj[key];
-})
+});
+var slice = Array.prototype.slice;
 
-// function _get(obj,key){
-//   return obj == null ? undefined : obj[key];
-// }
+// rest
+function _rest(list,num){
+  return slice.call(list,num || 1);
+}
+
+// reduce
+function _reduce(list,iter,memo){
+  if(arguments.length ==2){
+    memo = list[0];
+    list = _rest(list,1);
+  }
+  _each(list,function(val){
+    memo = iter(memo,val);
+  });
+  return memo
+}
+
+//  pipe
+function _pipe(){
+  var fns = arguments;
+  return function(arg){
+    return _reduce(fns,function(arg,fn){
+      return fn(arg);
+    },arg)
+  }
+}
+
+// go
+function _go(arg){
+  var fns = _rest(arguments);
+  return _pipe.apply(null, fns)(arg);
+}
+
+
+
+var _map = _curryr(_map),_filter = _curryr(_filter);
